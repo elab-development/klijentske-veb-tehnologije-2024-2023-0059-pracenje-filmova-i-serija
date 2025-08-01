ab54f5e550693af94b30e477d58ae050aa36b632
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2025-08-01 21:49:13 +0200

    Login page improvements

--- filmspot/app/routes/login.tsx
+++ filmspot/app/routes/login.tsx
@@ -16,7 +16,7 @@ export default function FilmSpotLogin() {
   };
 
   return (
-    <div className="min-h-screen flex items-start lg:items-center justify-center p-4">
+    <div className="min-h-screen flex items-start lg:items-center justify-center p-4 max-[600px]:px-0">
       
       <LightRays
             raysOrigin="right"
@@ -33,7 +33,7 @@ export default function FilmSpotLogin() {
         
       <div className="w-full max-w-6xl flex flex-col-reverse lg:grid lg:grid-cols-2 gap-5 lg:gap-15 items-center z-40 p-5">
         {/* Left side - Login Form */}
-        <div className="w-full bg-[var(--backgroundTransparentSecondary)] backdrop-blur-sm border border-[var(--borderColorSecondary)] rounded-2xl p-10 pb-20 shadow-2xl shrink-0">
+        <div className="w-full bg-[var(--backgroundTransparentSecondary)] backdrop-blur-sm border border-[var(--borderColorSecondary)] rounded-2xl p-10 pb-20 max-[600px]:p-6 shadow-2xl shrink-0">
           <div className="mb-8">
             <h1 className="text-3xl font-bold text-white mb-2">Sign in</h1>
             <p className="text-gray-400">
@@ -112,21 +112,21 @@ export default function FilmSpotLogin() {
         </div>
 
         {/* Right side - Welcome Section */}
-        <div className="text-center lg:text-left mb-5">
-          <div className="flex items-center justify-center lg:justify-start mb-6 gap-3">
+        <div className="text-center lg:text-left mb-5 max-[600px]:mb-0">
+          <div className="flex items-center justify-center lg:justify-start mb-6 max-[600px]:mb-0 gap-3">
             <svg className='ml-[-5px]' width="60" height="60" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.79 23.79C10.79 16.614 16.614 10.79 23.79 10.79H80.145C87.321 10.79 93.145 16.614 93.145 23.79V80.145C93.145 87.321 87.321 93.145 80.145 93.145H23.79C16.614 93.145 10.79 87.321 10.79 80.145V23.79Z" fill="white"/>
                 <path d="M21.684 31.837C21.476 27.534 24.804 23.881 29.107 23.673L72.1241 21.684C76.427 21.476 80.0801 24.804 80.2751 29.107L82.2771 72.1241C82.4721 76.427 79.1441 80.0801 74.8411 80.2751L31.837 82.2771C27.534 82.4721 23.881 79.1441 23.673 74.8411L21.684 31.837Z" fill="#43DFD7"/>
                 <path d="M31.005 37.388C41.665 35.4206 55.2933 33.9516 71.89 32.981C72.618 41.2663 72.7307 47.437 72.228 51.493C65.9273 51.6316 59.7523 52.0043 53.703 52.611L54.08 56.836C58.5867 56.3593 62.127 56.056 64.701 55.926C64.9523 58.656 64.9393 63.3446 64.662 69.992C61.386 70.2693 58.1533 70.5726 54.964 70.902C55.042 75.1313 55.237 79.846 55.549 85.046C49.959 85.982 44.6247 86.45 39.546 86.45C39.3293 85.4446 38.8137 74.5983 37.999 53.911L31.967 54.665C31.525 49.283 31.2043 43.524 31.005 37.388Z" fill="white"/>
             </svg>
-            <h2 className="text-4xl font-bold text-white">FilmSpot</h2>
+            <h2 className="text-4xl font-bold text-white"><span className='text-[#43DFD7]'>Film</span>Spot</h2>
           </div>
 
           <div className="space-y-4 text-slate-300">
-            <p className="text-lg leading-relaxed">
+            <p className="text-lg leading-relaxed max-[600px]:hidden">
               Welcome to the world of movies and TV shows! Create an account to keep a list of your favorite movies, rank and rate them. It's free, always.
             </p>
-            <p className="text-sm text-slate-400">
+            <p className="text-sm text-slate-400 max-[600px]:hidden">
               By signing in you accept our{' '}
               <button className="text-[#43DFD7] hover:text-cyan-300 transition-colors underline">
                 Terms of Use
