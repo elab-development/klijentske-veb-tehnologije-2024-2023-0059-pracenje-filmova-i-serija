62cf450c9b9a23bfe67c45c2afe1f57291564103
Mykers <Miki@qunitalstudioss.onmicrosoft.com>
2025-08-02 18:30:16 +0200

    Movie Page static design done
    - Design template is done for movie page

--- filmspot/app/components/ContentType.tsx
+++ filmspot/app/components/ContentType.tsx
@@ -1,6 +1,6 @@
 function ContentType({type, additionalClasses}: {type: number, additionalClasses?: string}){
     return (
-        <span className={`type${type === 0 ? '' : " series"} ${additionalClasses}`}>
+        <span className={`type${type === 0 ? '' : " series"} ${additionalClasses ?? ''}`}>
             {type === 0 ? "MOVIE" : "TV SHOW"}
         </span>
     );
--- filmspot/app/routes/movie.tsx
+++ filmspot/app/routes/movie.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router";
 import Footer from "~/components/Footer";
 import Header from "~/components/Header";
 import MovieInfoHolder from "~/components/moviePage/MovieInfoHolder";
+import SameGenreMovies from "~/components/moviePage/SameGenreMovies";
+import SimilarMovies from "~/components/moviePage/SimilarMovies";
 import type { MovieCardProps } from "~/types";
 
 function MoviePage(){
@@ -18,7 +20,11 @@ function MoviePage(){
 
         <main className="movieInfoHolder min-h-[100dvh] max-w-[1400px] mx-auto">
             {movieInfo
-                ? <MovieInfoHolder props={movieInfo} /> 
+                ? <>
+                    <MovieInfoHolder props={movieInfo} />
+                    <SimilarMovies /> 
+                    <SameGenreMovies /> 
+                </>
                 : <h2>Ucitavanje</h2>
             }
         </main>
