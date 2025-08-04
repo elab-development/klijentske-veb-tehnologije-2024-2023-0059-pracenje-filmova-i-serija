e82aaf8430ae32c0423b36df80fec555a3ad2783
Mykers <Miki@qunitalstudioss.onmicrosoft.com>
2025-08-04 19:13:28 +0200

    Added alternative profile pictures

--- filmspot/app/components/moviePage/CastItem.tsx
+++ filmspot/app/components/moviePage/CastItem.tsx
@@ -1,9 +1,11 @@
 import type { CastInfo } from "~/types";
+import Man from "app/assets/Man.png";
+import Woman from "app/assets/Man.png";
 
 function CastItem({props}: {props: CastInfo}){
     return <>
         <span className="castItem">
-            <img src={`${import.meta.env.VITE_TMDB_PROFILE_BASE_URL}${props.profile_path}`} alt="Profile" />
+            <img src={props.profile_path ? `${import.meta.env.VITE_TMDB_PROFILE_BASE_URL}${props.profile_path}` : props.gender === 1 ? Man : Woman} alt="Profile" />
 
             <span>
                 <h3>{props.name}</h3>
--- filmspot/app/components/moviePage/MovieInfoPart.tsx
+++ filmspot/app/components/moviePage/MovieInfoPart.tsx
@@ -1,6 +1,4 @@
 function MovieInfoPart({items, additionalClasses}: {items: string[], additionalClasses?: string}){
-    console.log("Genres: ", items)
-    
     const itemsList = items.map(item => {
         return <span className="type">{item}</span>
     })
