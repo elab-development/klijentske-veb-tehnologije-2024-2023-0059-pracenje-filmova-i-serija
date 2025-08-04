0ee4052d5632950ddefa254eee56ae395e49f1ff
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2025-08-04 21:02:26 +0200

    Few improvements
    - Added a hover animation to horizontal movie cards

--- filmspot/app/components/moviePage/SimilarOrRecommended.tsx
+++ filmspot/app/components/moviePage/SimilarOrRecommended.tsx
@@ -16,12 +16,20 @@ function SimilarOrRecommended({props}: {props: Props}){
     const more = moreContent?.map((item: MovieInfo) => {
         return <>
             <a href={`/content/${props.type}.${item.id}`} className="contentItem">
+                <span className="topShadow"></span>
+                
                 <img src={`${import.meta.env.VITE_TMDB_POSTER_BASE_URL}/${item.poster_path}`} alt="Banner" />
 
                 <span>
                     <h3>{item.title}</h3>
                     <ContentType type={props.type} />
                 </span>
+
+                <button className="bookmark absolute button z-1">
+                    <svg width="15" height="15" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 3C1 1.89543 1.89543 1 3 1H12C13.1046 1 14 1.89543 14 3V17.6779C14 18.5555 12.9505 19.0074 12.3129 18.4045L7.5 13.8529L2.68711 18.4045C2.04954 19.0074 1 18.5555 1 17.6779V3Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
 
                 <span className="bottomShadow"></span>
             </a>
