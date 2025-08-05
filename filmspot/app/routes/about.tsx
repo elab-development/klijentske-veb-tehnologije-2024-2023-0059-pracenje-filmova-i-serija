5b22c3e595f947365b7355a31a8b2406048a108f
Mykers <Miki@qunitalstudioss.onmicrosoft.com>
2025-08-05 15:32:49 +0200

    About page layout fix

--- filmspot/app/routes/about.tsx
+++ filmspot/app/routes/about.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return <>
         <Header />
 
-        <main className="w-full max-w-[1400px] mx-auto min-h-[80dvh]">
-            <p>Welcome to FilmSpot! We are a passionate team of film enthusiasts dedicated to sharing the love of the seventh art. Our goal is to provide you with the latest information about movies, including descriptions, trailers and reviews. Enjoy exploring the world of film with us and find your next favorite title. FilmSpot - Where passion and film meet!</p>
+        <main className="w-full max-w-[1400px] mx-auto">
+            <p className="mt-5">Welcome to FilmSpot! We are a passionate team of film enthusiasts dedicated to sharing the love of the seventh art. Our goal is to provide you with the latest information about movies, including descriptions, trailers and reviews. Enjoy exploring the world of film with us and find your next favorite title. FilmSpot - Where passion and film meet!</p>
             <p className="mt-5">Contact: <a className="text-[var(--textAccentColor)]" href="mailto:contact@filmspot.com">contact@filmspot.com</a></p>
         </main>
 
